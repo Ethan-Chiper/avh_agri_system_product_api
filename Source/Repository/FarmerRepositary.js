d4567ece71e0ca2b597fcb3d71276b5f88cfa3c7
Arulmani <arulmani.v@ippopay.com>
2023-09-12 17:03:45 +0530

    initial commit

--- Source/Repository/FarmerRepositary.js
+++ Source/Repository/FarmerRepositary.js
@@ -22,7 +22,13 @@ const FormerQuery = {
         return await FarmerModel[condition.method](condition, projection).lean(islean);
 
     },
-
+    /***
+     * To do find all query
+     * @param condition
+     * @param projection
+     * @param islean
+     * @returns {Promise<awaited Query<Array<THydratedDocumentType> extends null ? (GetLeanResultType<* extends Schema<infer EnforcedDocType, infer M, infer TInstanceMethods, infer TQueryHelpers, infer TVirtuals, infer TStaticMethods, infer TSchemaOptions, infer DocType> ? DocType : unknown, Array<THydratedDocumentType>, "find"> | null) : GetLeanResultType<* extends Schema<infer EnforcedDocType, infer M, infer TInstanceMethods, infer TQueryHelpers, infer TVirtuals, infer TStaticMethods, infer TSchemaOptions, infer DocType> ? DocType : unknown, Array<THydratedDocumentType>, "find">, THydratedDocumentType, * extends Schema<infer EnforcedDocType, infer M, infer TInstanceMethods, infer TQueryHelpers, infer TVirtuals, infer TStaticMethods, infer TSchemaOptions, infer DocType> ? TQueryHelpers : unknown, * extends Schema<infer EnforcedDocType, infer M, infer TInstanceMethods, infer TQueryHelpers, infer TVirtuals, infer TStaticMethods, infer TSchemaOptions, infer DocType> ? DocType : unknown, "find"> & * extends Schema<infer EnforcedDocType, infer M, infer TInstanceMethods, infer TQueryHelpers, infer TVirtuals, infer TStaticMethods, infer TSchemaOptions, infer DocType> ? TQueryHelpers : unknown>}
+     */
     findFarmer: async (condition, projection, islean = true) => {
         if (isEmpty(projection)) {
             projection = {
