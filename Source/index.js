eb68d896a42a0199899ccb1596522e316e72b536
Arulmani <arulmani.v@ippopay.com>
2023-09-12 18:06:28 +0530

    initial commit

--- Source/index.js
+++ Source/index.js
@@ -5,10 +5,10 @@ App.use(helmet.hidePoweredBy());
 const bodyParser = require('body-parser');
 
 App.use(bodyParser.json());
-
+/***----------------------------------------------------------------------------------------------**/
 App.use('/api/product', require('./Routes/ProductRouter'));
 App.use('/api/former', require('./Routes/FarmerRouter'));
-
+/***----------------------------------------------------------------------------------------------**/
 require('./Models/MultiConnection').establish(App);
 
 module.exports = App;
